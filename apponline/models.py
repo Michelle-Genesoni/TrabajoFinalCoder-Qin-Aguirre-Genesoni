d50a29b8bb54677bc85dabf74a0d7e0eaf9278ec
Michelle Genesoni <GenesoniMishuu@gmail.com>
2022-09-29 01:00:03 -0300

    agregado de migracion de avatar

--- apponline/models.py
+++ apponline/models.py
@@ -1,5 +1,6 @@
 from pickle import TRUE
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Carteras(models.Model):
     nombre = models.CharField(max_length=128)
@@ -33,6 +34,11 @@ class Accesorios(models.Model):
 
     def __str__(self):
         return f'{self.codigo}, {self.nombre}, {self.stock}' 
+
+class Avatar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    imagen = models.ImageField(upload_to='avatares', null=True, blank = True)
+    
         
 
 
